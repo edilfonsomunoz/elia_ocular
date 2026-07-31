986ee309c000613d31ed6475681fc282fe5c4c96
edilfonsomunoz <edilfonsomunoz@users.noreply.github.com>
2026-07-31 10:28:50 -0500

    Fix dataset upload to use api instance with baseURL (VITE_API_URL)

--- frontend/src/api/plant.js
+++ frontend/src/api/plant.js
@@ -1,5 +1,4 @@
 import api from './axios';
-import axios from 'axios';
 
 export const getPlantOverview = () => api.get('/plant/overview').then(r => r.data);
 export const getPlantClasses = () => api.get('/plant/classes').then(r => r.data);
@@ -10,14 +9,7 @@ export const getPlantConfusionMatrix = () => api.get('/plant/confusion-matrix').
 export const getPlantClassificationReport = () => api.get('/plant/classification-report').then(r => r.data);
 export const getPlantPredictions = () => api.get('/plant/predictions').then(r => r.data);
 
-export const uploadDataset = (formData) => {
-  const token = localStorage.getItem('token');
-  return axios.post('/api/v1/plant/upload', formData, {
-    headers: {
-      ...(token ? { Authorization: `Bearer ${token}` } : {})
-    }
-  }).then(r => r.data);
-};
+export const uploadDataset = (formData) => api.post('/plant/upload', formData).then(r => r.data);
 
 export const listDatasets = () => api.get('/plant/datasets').then(r => r.data);
 export const deleteDataset = (id) => api.delete(`/plant/datasets/${id}`).then(r => r.data);
